ebf1e7454341face237cb83963ba96f625f6d0ed
Ryan Cleven <rcleven@mac.com>
2024-05-11 12:01:07 -0700

    Initial add 2

--- src/h-box2c.h
+++ src/h-box2c.h
@@ -1,6 +1,15 @@
 #pragma once
 
+#include "box2d/box2d.h"
+#include "box2d/callbacks.h"
+#include "box2d/hull.h"
+#include "box2d/manifold.h"
+#include "box2d/math.h"
+#include "box2d/math_cpp.h"
+#include "box2d/timer.h"
+
+
 namespace hbox2c
 {
-    
+
 }
