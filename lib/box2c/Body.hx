89a97796e0703f5b318688ed1a9a607e41c70d3a
Ryan Cleven <rcleven@mac.com>
2024-05-18 01:50:12 -0700

    added more comfortable abstraction

---
+++ lib/box2c/Body.hx
@@ -0,0 +1,16 @@
+package box2c;
+import box2c.Native.WorldContext;
+import box2c.Native.BodyDef;
+import box2c.Native.BodyType;
+import box2c.Native.World;
+import box2c.Native.ShapeDef;
+import box2c.Native.BodyFunc;
+abstract Body(haxe.Int64) from haxe.Int64 to haxe.Int64 {
+    public inline function createCircleShape( def : ShapeDef, center_x : Float, center_y : Float, radius : Float) : haxe.Int64 {
+        return BodyFunc.createCircleShape(this, def, center_x, center_y, radius);
+    }
+
+    public inline function setTransform( x : Float, y : Float, angle : Float) : Void {
+        return BodyFunc.setTransform(this, x, y, angle);
+    }
+}
--- test/Test.hx
+++ test/Test.hx
@@ -4,7 +4,7 @@ import box2c.Native.BodyDef;
 import box2c.Native.BodyType;
 import box2c.Native.World;
 import box2c.Native.ShapeDef;
-import box2c.Native.Body;
+import box2c.Body;
 
 class Test {
 
@@ -21,13 +21,14 @@ class Test {
             var bodyDef = new BodyDef();
 			// b2BodyDef bodyDef = b2DefaultBodyDef();
 			bodyDef.type = BodyType.dynamicBody;
-            var bodyId = World.createBody(world, bodyDef);
+            var body : Body = World.createBody(world, bodyDef);
 			// bodyDef.position = b2Body_GetPosition(m_ship1Id);
 			// bodyDef.angle = b2Body_GetAngle(m_ship1Id);
 			// // bodyDef.gravityScale = 0.0f;
 
             var shapeDef = new ShapeDef();
-            Body.createCircleShape(bodyId, shapeDef, 0.5, 0.0, 2.0);
+            body.createCircleShape( shapeDef, 0.5, 0.0, 2.0);
+            body.setTransform( 0.0, 10.0, 0.0);
 		}
 
 
